e5566a4da5dba900fed33f76ef8bdfcfba5425c0
Alexander <voron-1998-va@mail.ru>
2021-08-20 16:48:34 +0900

    modifed a game grid

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+import sys
 import turtle as t
 from random import randint
-from copy import deepcopy
 from time import sleep
+
+
 # field settings (настройки поля)
 cell = 50
 # количество ячеек по ширине
@@ -10,11 +12,14 @@ width = 16
 length = 12
 
 window = t.Screen()
-window.setup(cell * width + 2, cell * length + 2)
+window.setup(cell * width + 120, cell * length + 120)
 
-window._root.resizable(False, False)
+
+# window._root.resizable(False, False)
 window.bgcolor('black')
 window.title('CODDY snake')
+window.tracer(0)
+
 
 # строится сетка на поле
 # grid - сетка
@@ -22,32 +27,39 @@ grid = t.Turtle()
 grid.color('white')
 grid.speed(0)
 grid.penup()
-grid.goto(-0.5 * cell * width + cell, 0.5 * cell * length)
-grid.pendown()
+grid.goto(-0.5 * cell * width, 0.5 * cell * length)
 
 
-for i in range(0, width - 1):
+
+for i in range(0, width + 1):
+    grid.pendown()
     if i % 2 == 0:
         directionx = -1
     elif i % 2 == 1:
         directionx = 1
 
-    grid.sety(cell * length * directionx)
+    grid.sety(0.5 * cell * length * directionx)
+    grid.penup()
     grid.forward(cell)
 
 
 grid.penup()
-grid.goto(-0.5 * cell * width, 0.5 * cell * length - cell)
-grid.pendown()
+grid.goto(-0.5 * cell * width, 0.5 * cell * length)
 
-for i in range(0, length - 1):
+
+
+
+
+for i in range(0, length + 1):
+    grid.pendown()
     if i % 2 == 0:
         grid.forward(cell * width)
-
     elif i % 2 == 1:
         grid.backward(cell * width)
 
+    grid.penup()
     grid.sety(grid.pos()[1] - cell)
+grid.hideturtle()
 
 # spawn an apple
 apple = t.Turtle()
@@ -56,8 +68,27 @@ apple.shape('square')
 apple.shapesize(2)
 apple.penup()
 apple.hideturtle()
-apple.goto(randint(-int(width / 2 - 1), int(width / 2 - 1)) * cell + cell / 2, randint(-int(length / 2 - 1), int(length / 2 - 1)) * cell + cell / 2)
-apple.showturtle()
+
+def spawn_apple(snake):
+
+    while True:
+        counter = 0
+        x = randint(-int(width / 2 - 1), int(width / 2 - 1))
+        y = randint(-int(length / 2 - 1), int(length / 2 - 1))
+
+        x_apple = x * cell + cell / 2
+        y_apple = y * cell + cell / 2
+
+        for i in snake:
+            if (x_apple == i.pos()[0] and y_apple == i.pos()[1]):
+                counter += 1
+                break
+        if counter == 0:
+            break
+
+    apple.goto(x_apple, y_apple)
+    apple.showturtle()
+
 
 
 
@@ -104,25 +135,33 @@ snake = [snake_head, snake_body1, snake_body2, snake_body3]
 snakex = -1
 snakey = 0
 
+
+spawn_apple(snake)
+
+
 def go_up():
     global snakex, snakey
-    snakex = 0
-    snakey = 1
+    if snakey != -1:
+        snakex = 0
+        snakey = 1
 
 def go_down():
     global snakex, snakey
-    snakex = 0
-    snakey = -1
+    if snakey != 1:
+        snakex = 0
+        snakey = -1
 
 def go_right():
     global snakex, snakey
-    snakex = 1
-    snakey = 0
+    if snakex != -1:
+        snakex = 1
+        snakey = 0
 
 def go_left():
     global snakex, snakey
-    snakex = -1
-    snakey = 0
+    if snakex != 1:
+        snakex = -1
+        snakey = 0
 
 t.onkey(go_up, 'Up')
 t.onkey(go_down, 'Down')
@@ -130,33 +169,155 @@ t.onkey(go_right, 'Right')
 t.onkey(go_left, 'Left')
 
 
+def add_block():
+    """increase the length of the snake"""
+    block = t.Turtle()
+    block.shape('square')
+    block.color('green')
+    block.shapesize(2)
+    block.hideturtle()
+    block.penup()
+
+    return block
 
 
-
-
-
-
+window.update()
+window.tracer(1)
 
 while True:
 
+    # создаем список координат каждого блока змейки (предыдущие координаты)
     coords = []
     for i in snake:
         coords.append(i.pos())
 
 
+    if ((snake_head.pos()[0] + cell * snakex) - (cell / 2)) < -(width / 2) * cell:
+        sys.exit()
+
+    if ((snake_head.pos()[0] + cell * snakex) + (cell / 2)) > (width / 2) * cell:
+        sys.exit()
+
+
+    if ((snake_head.pos()[1] + cell * snakey) + (cell / 2)) > (length / 2) * cell:
+        sys.exit()
+
+
+    if ((snake_head.pos()[1] + cell * snakey) - (cell / 2)) < -(length / 2) * cell:
+        sys.exit()
+
     snake_head.setpos(snake_head.pos()[0] + cell * snakex, snake_head.pos()[1] + cell * snakey)
 
 
+
+
+
     for index, part in enumerate(snake[1:]):
+        part.goto(coords[index])
 
-        if index == 0:
-            part.goto(coords[0])
-        else:
-            part.goto(coords[index])
 
-    sleep(0.1)
+
+
+
+    if (snake[0].pos()[0] == apple.pos()[0]) and (snake[0].pos()[1] == apple.pos()[1]):
+        apple.hideturtle()
+
+        if snake[-1].pos()[1] == snake[-2].pos()[1]:
+            # змейка движется вправо
+            if snakex == 1:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] - cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+
+            # змейка движется влево
+            elif snakex == -1:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] + cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+
+        if snake[-1].pos()[0] == snake[-2].pos()[0]:
+            # змейка движется вверх
+            if snakey == 1:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0], snake[-1].pos()[1] - cell)
+                snake_part.showturtle()
+                snake.append(snake_part)
+            # змейка движется вниз
+            elif snakey == -1:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0], snake[-1].pos()[1] + cell)
+                snake_part.showturtle()
+                snake.append(snake_part)
+
+
+        if (snake[0].pos()[1] == 1) and (snake[-2].pos()[1] == snake[-1].pos()[1]):
+
+            if snake[-1].pos()[0] < snake[-2].pos()[0]:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] - cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+            elif snake[-1].pos()[0] > snake[-2].pos()[0]:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] + cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+
+
+        if ((snake[0].pos()[0] == -1) or (snake[0].pos()[0] == 1))  and (snake[-2].pos()[0] == snake[-1].pos()[0]):
+            snake_part = add_block()
+            snake_part.setpos(snake[-1].pos()[0], snake[-1].pos()[1] + cell)
+            snake_part.showturtle()
+            snake.append(snake_part)
+
+
+        if (snake[0].pos()[1] == -1) and (snake[-2].pos()[1] == snake[-1].pos()[1]):
+
+            if snake[-1].pos()[0] < snake[-2].pos()[0]:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] - cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+            elif snake[-1].pos()[0] > snake[-2].pos()[0]:
+                snake_part = add_block()
+                snake_part.setpos(snake[-1].pos()[0] + cell, snake[-1].pos()[1])
+                snake_part.showturtle()
+                snake.append(snake_part)
+
+        spawn_apple(snake)
+
+
+
+
+
+
+
+
 
     t.listen()
-
+    sleep(0.15)
 
 t.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
